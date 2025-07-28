5b2ac74dd2888d72f3d791dfdba94ea1822d7f4f
Muhammadou Drammeh <mjdrammeh1@bakerripley.org>
2025-07-28 10:51:41 -0500

    Added Dictionary

---
+++ AddTwoSum/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace AddTwoSum
+{
+    internal class Program
+    {
+        /**
+         * Given an array of integers nums and an integer target,
+         * return indices of the two numbers such that they add up to target.
+         */
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 26;
+
+            AddTwoSum(nums, target);
+            Console.WriteLine($"{nums[2]} + {nums[3]} = {target}");
+            Console.WriteLine(string.Join(", ", $"{nums[2] + nums[3] == target}"));
+
+
+
+        }
+
+
+        public static int[] AddTwoSum(int[] nums, int target)
+        {
+
+            // have two numbers array that add to the target
+            // nested loop to go iterate through every value pair
+            // check to see if any value pair == to target
+
+            // first iteration with i from index 0 to end n-1
+            // second iteraion with j from index i + 1 to end n-1 to find to look for possible values of num[i]
+            // check if i array + j array = target 
+            //return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return [i, j];
+                    }
+                }
+            }
+            Console.WriteLine();
+
+            return nums;
+
+
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
---
+++ DictionaryC#/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DictionaryC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int , string> fruitCount = new Dictionary<int , string>();
+
+            fruitCount.Add(1, "Apple");
+            fruitCount.Add(2, "Mangoe");
+            fruitCount.Add(3, "Orange");
+            fruitCount[4] = "Banana";
+
+            fruitCount.Add(5, "Kiwi");
+
+            foreach (KeyValuePair<int, string> input in fruitCount)
+            {
+                Console.WriteLine($"Id:{input.Key} Fruit:{input.Value} ");
+            }
+
+
+
+
+
+        }
+    }
+}
